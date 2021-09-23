2cdea9d736f8e659b83ef800f122fb79b9b0e5e8
Moses <moses.junior@student.moringaschool.com>
2021-09-23 13:07:07 +0300

    editting gitignore file

--- config.py
+++ config.py
@@ -5,6 +5,12 @@ class Config:
     '''
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moses:1234@localhost/pitchbook'
     SECRET_KEY= os.environ.get('SECRET_KEY')
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
 
